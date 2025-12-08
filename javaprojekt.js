8f2654f1d3cae2ec7ba4ef0a937d74d5ff6bd95b
emaniooo <eman.abubeker@yh.nackademin.se>
2025-12-09 00:25:45 +0100

    added DOM elements

---
+++ javaprojekt.js
@@ -0,0 +1,5 @@
+let quizForm = document.getElementById("quizForm");
+let submitBtn = document.getElementById("submitBtn");
+let resultDiv = document.getElementById("result");
+let introBox = document.getElementById("introBox");
+let toggleMode = document.getElementById("toggleMode");
