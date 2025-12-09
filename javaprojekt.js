bfb061fb8a1d402e4555b09590d72fe3c5723f00
emaniooo <eman.abubeker@yh.nackademin.se>
2025-12-09 01:21:42 +0100

    added the CRY method

--- javaprojekt.js
+++ javaprojekt.js
@@ -8,3 +8,26 @@ document.getElementById("startQuizBtn").addEventListener("click", () => {
     document.getElementById("quizForm").style.display = "block";
     document.getElementById("submitBtn").style.display = "block";
 });
+
+// RÄKNA RESULTAT (DRY)
+const submitBtn = document.getElementById("submitBtn");
+const resultDiv = document.getElementById("result");
+
+submitBtn.addEventListener("click", () => {
+    let score = 0;
+
+    // Hämta alla frågor
+    const questions = document.querySelectorAll(".question");
+
+    questions.forEach((q) => {
+        const type = q.dataset.type;          // tf, mc, cb
+        const correct = q.dataset.correct;    // t.ex. "1", "sant", "0,2,3"
+        const labels = q.querySelectorAll("label");
+
+    });
+
+
+
+
+    
+});
